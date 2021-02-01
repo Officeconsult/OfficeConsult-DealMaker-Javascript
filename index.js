312069fd7a5365c9f02d239a767686d806531691
internoffice <61001730+internoffice@users.noreply.github.com>
2021-02-01 15:24:26 +0100

    added document cookies and request to airtable

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ var key;
 
 function setKey() {
     key = $("input.airTablePrivateKey").val();
-    var alertString = "you've entered the following key: ";
-    alertString += key;
-    alert(alertString);
-
     document.cookie = "key=" + key;
+}
+
+async function displayBase(){
+    const url = "https://api.airtable.com/v0/appPu05N346JHlDTg/Tilbud";
+
+    const options = {
+      headers: {
+        Authorization: "Bearer " + key
+      }
+    };
+    
+    var resp;
+    await fetch(url, options).then( res => resp = res.json());
+    console.log(resp);
 }
 
 function showKey() {
